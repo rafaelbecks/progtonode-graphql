687cd5439297b443d3e9f1201bb56f8e2ac80b43
Rafael Angel Becerra Blasco <rbecerra@belatrix.com>
2019-07-16 16:53:18 -0500

    style(queries): code formatting, spacing

--- src/resolvers/queries/index.js
+++ src/resolvers/queries/index.js
@@ -2,9 +2,11 @@ import { search, getArtist } from '../../api';
 import { graphBuilder, getGraph} from './graphBuilder';
 
 export const queriesResolver = {
+
   async artistSearch(parents, { query, type, perPage, page}, context){
     return await search(query, { type, perPage, page });
   },
+
   async graphConstruct(parents, { artistId, level }, context){
 
     return await getGraph(artistId, level);
